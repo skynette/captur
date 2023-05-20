cc6da8755d40152613eff7cb92cf60387c4ba371
Joshua <cutejosh2@gmail.com>
2023-05-20 22:55:14 +0100

    image slider

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,16 +1,40 @@
+'use client'
 import Image from "next/image"
+import { useState } from "react"
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa"
 
 const Slider = ({ slides }) => {
+	const [current, setCurrent] = useState(0)
+	const length = slides.length
+
+	const nextSlide = () => {
+		setCurrent(current === length - 1 ? 0 : current + 1)
+	}
+	const prevSlide = () => {
+		setCurrent(current === 0 ? length - 1 : current - 1)
+	}
+
+	if (!Array.isArray(slides) || slides.length <= 0) {
+		return null
+	}
+
 	return (
-		<div id="gallery">
-			<h1>Gallery</h1>
-			<div>
-				{slides.map((slide, index) => {
-					return (
-						<Image key={index} src={slide.image} alt={slide.alt} width="1440" height="600" objectFit="cover"/>
-					)
-				})}
-			</div>
+		<div id="gallery" className="max-w-[1240px] mx-auto">
+			<h1 className="text-2xl font-bold text-center p-4">Gallery</h1>
+			{slides.map((slide, index) => {
+				return (
+					<div key={index} className={index === current ? 'opacity-[1] ease-in duration-1000' : 'opacity-0'}>
+						<div className="relative flex justify-center p-4">
+							<FaArrowAltCircleLeft className="absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]" size={50} onClick={nextSlide} />
+							{index === current && (
+								<Image src={slide.image} alt={slide.alt} width="1440" height="600" objectFit="cover" />
+							)}
+							<FaArrowAltCircleRight className="absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]" size={50} onClick={prevSlide} />
+						</div>
+					</div>
+				)
+			})}
+
 		</div>
 	)
 }
--- components/SliderData.js
+++ components/SliderData.js
@@ -3,15 +3,15 @@ export const SliderData = [
 		image: 'https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGhvdG9ncmFwaHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
 	},
 	{
-		image: 'https://plus.unsplash.com/premium_photo-1664701475272-953393050754?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGhvdG9ncmFwaHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+		image: 'https://images.unsplash.com/photo-1464855499786-2839659d9e5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80'
 	},
 	{
-		image: 'https://images.unsplash.com/photo-1607462109225-6b64ae2dd3cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+		image: 'https://images.unsplash.com/photo-1541515929569-1771522cbaa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
 	},
 	{
-		image: 'https://images.unsplash.com/photo-1590071089561-2087ff1fc418?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+		image: 'https://images.unsplash.com/photo-1580892047528-7dfd384dce65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80'
 	},
 	{
-		image: 'https://images.unsplash.com/photo-1621600411688-4be93cd68504?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+		image: 'https://images.unsplash.com/photo-1452587925148-ce544e77e70d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80'
 	},
 ]
